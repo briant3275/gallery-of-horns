73735c55c349cbfed9bbfeab0127515b55e4706e
Brian Thornburg <briant3275@gmail.com>
2022-02-16 22:45:58 -0600

    fixed img alt property

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -19,7 +19,7 @@ class SelectedBeast extends React.Component {
                     <Modal.Title>{this.props.beast.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <img src={this.props.beast.image_url} className="img-fluid"/>
+                    <img src={this.props.beast.image_url} alt={this.props.beast.title} className="img-fluid"/>
                     {this.props.beast.description}
                 </Modal.Body>
 
